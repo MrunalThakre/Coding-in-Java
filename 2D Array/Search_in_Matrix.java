9df526d890d5c751ad12ced77cc77ec5eaab501d
MrunalThakre <101035846+MrunalThakre@users.noreply.github.com>
2024-01-28 23:44:46 +0530

    Add files via upload

---
+++ 2D Array/Search_in_Matrix.java
@@ -0,0 +1,14 @@
+public class Search_in_Matrix {
+    
+    public static void search(int matrix[][], int key){
+
+    }
+    public static void main(String[] args) {
+        int matrix[][]={{1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}};
+
+                        search(matrix, 11);
+    }
+}
