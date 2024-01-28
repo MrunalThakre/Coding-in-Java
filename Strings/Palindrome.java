683070e493591a5b5e4fe55971cf31780b52c337
MrunalThakre <101035846+MrunalThakre@users.noreply.github.com>
2024-01-28 23:46:14 +0530

    Add files via upload

---
+++ Strings/Palindrome.java
@@ -0,0 +1,16 @@
+public class Palindrome {
+    
+    public static boolean check(String str){
+        for(int i=0; i<str.length()/2; i++){
+            int n=str.length();
+            if(str.charAt(i)!=str.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str= "car";
+        check(str);
+    }
+}
